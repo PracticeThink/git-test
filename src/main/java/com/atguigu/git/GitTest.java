31852d99384d7fe2d9eebea361648f44a66881ee
lzs <lzs@qq.com>
2022-02-15 23:07:02 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,change");
         System.out.println("hello,change again");
         System.out.println("hello,hot-fix change");
+        System.out.println("hello,master change again");
     }
 }
