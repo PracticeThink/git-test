bb8c3c9a743497f350b6fd3d7abb35a410c41471
PracticeThink <90978650+PracticeThink@users.noreply.github.com>
2022-02-15 23:33:09 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,master change again");
         System.out.println("hello,hot-fix change again");
         System.out.println("test push");
+        System.out.println("test pull");
     }
 }
